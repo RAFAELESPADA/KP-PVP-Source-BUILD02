5fe0df93595bf0ddf7ea8ade11cc9b65e0e88809
RAFAELESPADA <34714331+RAFAELESPADA@users.noreply.github.com>
2018-01-03 00:51:39 -0200

    Update NoHunger.java

--- src/me/RafaelAulerDeMeloAraujo/main/NoHunger.java
+++ src/me/RafaelAulerDeMeloAraujo/main/NoHunger.java
@@ -3,6 +3,7 @@
 /*    */ import org.bukkit.entity.Player;
 /*    */ import org.bukkit.event.EventHandler;
 /*    */ import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 /*    */ import org.bukkit.event.entity.FoodLevelChangeEvent;
 
 import me.RafaelAulerDeMeloAraujo.SpecialAbility.Join;
@@ -10,20 +11,42 @@ import me.RafaelAulerDeMeloAraujo.main.Main;
 /*    */ 
 /*    */ public class NoHunger implements Listener
 /*    */ {
-	   private Main main;
+	  
 /*    */   public NoHunger(Main main) {}
 /*    */   
 /*    */   @EventHandler
 /*    */   public void onFall(FoodLevelChangeEvent e) {
 /*    */   
-	
+	if (!(e.getEntity() instanceof Player)) {
+        return;
+    }
 /* 15 */     
-/* 16 */     if ((Main.plugin.getConfig().getString("DisableHunger").equalsIgnoreCase("true")) && (Join.game.contains(e.getEntity()))) 
+/* 16 */     if ((Join.game.contains(e.getEntity()))) 
 /* 17 */       e.setCancelled(true);
-else
+{
+if ((!Join.game.contains(e.getEntity())))
 	 e.setCancelled(false);
 /*    */     }
-/*    */   }
-/*    */ 
+}
+/*    */   
+/*    */ @EventHandler
+public void damage2(final EntityDamageByEntityEvent e) {
+    if (!(e.getEntity() instanceof Player)) {
+        return;
+    }
+        if (!(e.getDamager() instanceof Player)) {
+            return;
+    }
+    final Player p = (Player)e.getEntity();
+    final Player k = (Player)e.getDamager();
+    if (Join.game.contains(p) && (Join.game.contains(k))) 
+        p.setFoodLevel(20);
+    else
+    	return;
+    }
+{
+}
+}
+
 
 

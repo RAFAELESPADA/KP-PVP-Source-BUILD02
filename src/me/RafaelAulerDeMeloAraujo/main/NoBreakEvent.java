a1c86d78a1ef3bdeb6ba8d7fbdb300d21fcc9543
RAFAELESPADA <34714331+RAFAELESPADA@users.noreply.github.com>
2018-03-30 15:14:12 -0300

    Update NoBreakEvent.java

--- src/me/RafaelAulerDeMeloAraujo/main/NoBreakEvent.java
+++ src/me/RafaelAulerDeMeloAraujo/main/NoBreakEvent.java
@@ -1,38 +1,44 @@
-package me.RafaelAulerDeMeloAraujo.main;
+/*    */ package me.RafaelAulerDeMeloAraujo.main;
+/*    */ 
+/*    */ import java.util.ArrayList;
+/*    */ import me.RafaelAulerDeMeloAraujo.SpecialAbility.Join;
+/*    */ import org.bukkit.entity.Player;
+/*    */ import org.bukkit.event.EventHandler;
+/*    */ import org.bukkit.event.Listener;
+/*    */ import org.bukkit.event.block.BlockBreakEvent;
+/*    */ import org.bukkit.event.block.BlockPlaceEvent;
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ public class NoBreakEvent
+/*    */   implements Listener
+/*    */ {
+/*    */   @EventHandler
+/*    */   public void aoconstruir(BlockPlaceEvent e)
+/*    */   {
+/* 19 */     Player p = e.getPlayer();
+/* 20 */     if ((Join.game.contains(p.getName())) && (!p.hasPermission("kitpvp.build")))
+/*    */     {
+/* 22 */       e.setCancelled(true);
+/*    */     } else {
+/* 24 */       e.setCancelled(false);
+/*    */     }
+/*    */   }
+/*    */   
+/*    */   @EventHandler
+/*    */   public void aoconstruir2(BlockBreakEvent e) {
+/* 30 */     Player p = e.getPlayer();
+/* 31 */     if ((Join.game.contains(p.getName())) && (!p.hasPermission("kitpvp.build")))
+/*    */     {
+/* 33 */       e.setCancelled(true);
+/*    */     } else {
+/* 35 */       e.setCancelled(false);
+/*    */     }
+/*    */   }
+/*    */ }
 
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
 
-import me.RafaelAulerDeMeloAraujo.SpecialAbility.Join;
-
-
-
-public class NoBreakEvent implements Listener {
-
-
-@EventHandler
-public void aoconstruir(final BlockPlaceEvent e) {
-	
-    final Player p = e.getPlayer();
-    if (Join.game.contains(p.getName()) && (!p.hasPermission("kitpvp.build")))   
-    	
-        e.setCancelled(true);
-    else
-    	e.setCancelled(false);
-    }
-   
-	
-@EventHandler
-public void aoconstruir2(final BlockBreakEvent e) {
-	final Player p = e.getPlayer();
-	  if (Join.game.contains(p.getName()) && (!p.hasPermission("kitpvp.build")))   
-	    	
-	        e.setCancelled(true);
-	    else
-	    	e.setCancelled(false);
-	    }
-}
-
+/* Location:              D:\Desktop\video\Minhas Coisas do Desktop\KP-PVPvB12 (1).jar!\me\RafaelAulerDeMeloAraujo\main\NoBreakEvent.class
+ * Java compiler version: 8 (52.0)
+ * JD-Core Version:       0.7.1
+ */

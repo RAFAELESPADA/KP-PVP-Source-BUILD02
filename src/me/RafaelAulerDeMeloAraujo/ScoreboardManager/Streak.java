5b03f5a68eb1fff10e58da0dd00ee42c35af0e3f
RAFAELESPADA <34714331+RAFAELESPADA@users.noreply.github.com>
2018-03-30 15:38:47 -0300

    Update Streak.java

--- src/me/RafaelAulerDeMeloAraujo/ScoreboardManager/Streak.java
+++ src/me/RafaelAulerDeMeloAraujo/ScoreboardManager/Streak.java
@@ -1,135 +1,140 @@
-package me.RafaelAulerDeMeloAraujo.ScoreboardManager;
+/*     */ package me.RafaelAulerDeMeloAraujo.ScoreboardManager;
+/*     */ 
+/*     */ import java.util.ArrayList;
+/*     */ import java.util.HashMap;
+/*     */ import java.util.Map;
+/*     */ import me.RafaelAulerDeMeloAraujo.Coins.Coins;
+/*     */ import me.RafaelAulerDeMeloAraujo.SpecialAbility.Join;
+/*     */ import me.RafaelAulerDeMeloAraujo.main.Main;
+/*     */ import org.bukkit.Bukkit;
+/*     */ import org.bukkit.ChatColor;
+/*     */ import org.bukkit.Sound;
+/*     */ import org.bukkit.configuration.file.FileConfiguration;
+/*     */ import org.bukkit.entity.Player;
+/*     */ import org.bukkit.event.EventHandler;
+/*     */ import org.bukkit.event.Listener;
+/*     */ import org.bukkit.event.entity.PlayerDeathEvent;
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ public class Streak
+/*     */   implements Listener
+/*     */ {
+/*     */   private Main main;
+/*     */   static Main plugin;
+/*     */   
+/*     */   public Streak()
+/*     */   {
+/*  31 */     this.main = this.main;
+/*  32 */     plugin = this.main;
+/*     */   }
+/*     */   
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*  40 */   public static Map<String, Integer> killstreak = new HashMap();
+/*     */   
+/*     */   public void onEnable() {}
+/*     */   
+/*     */   @EventHandler
+/*  45 */   public void playerdeath(PlayerDeathEvent ev) { Player p = ev.getEntity();
+/*  46 */     if ((p.getKiller() instanceof Player))
+/*     */     {
+/*     */ 
+/*  49 */       Player killer = p.getKiller();
+/*  50 */       if (Join.game.contains(killer))
+/*     */       {
+/*  52 */         p.sendMessage(ChatColor.RED + "Your killstreak has been destroyed by " + ChatColor.YELLOW + killer.getName()); }
+/*  53 */       addtokillstreak(killer);
+/*  54 */       killstreak.put(p.getName(), Integer.valueOf(0));
+/*     */     }
+/*     */   }
+/*     */   
+/*     */ 
+/*     */   public void addtokillstreak(Player killer)
+/*     */   {
+/*  61 */     String name = killer.getName();
+/*  62 */     if (killstreak.containsKey(name))
+/*     */     {
+/*  64 */       int kills = ((Integer)killstreak.get(name)).intValue();
+/*  65 */       kills++;
+/*  66 */       killstreak.put(name, Integer.valueOf(kills));
+/*     */       
+/*  68 */       killer.sendMessage(ChatColor.GREEN + "You are on " + ChatColor.RED + Integer.toString(kills) + ChatColor.GREEN + " Killstreak.");
+/*  69 */       if (kills == 100)
+/*     */       {
+/*  71 */         Bukkit.broadcastMessage(String.valueOf(ChatColor.GREEN + killer.getName() + 
+/*  72 */           ChatColor.DARK_GREEN + " is on a 100 killstreak!"));
+/*  73 */         killer.sendMessage(ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-100") + " Coins!");
+/*  74 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-100"));
+/*     */       }
+/*  76 */       if (kills == 80)
+/*     */       {
+/*  78 */         Bukkit.broadcastMessage(ChatColor.GREEN + killer.getName() + 
+/*  79 */           ChatColor.DARK_GREEN + " is on a 80 killstreak!");
+/*  80 */         killer.sendMessage(ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-80") + " Coins!");
+/*  81 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-80"));
+/*     */       }
+/*  83 */       if (kills == 65)
+/*     */       {
+/*  85 */         Bukkit.broadcastMessage(ChatColor.GREEN + killer.getName() + 
+/*  86 */           ChatColor.DARK_GREEN + " is on a 65 killstreak!");
+/*  87 */         killer.sendMessage(ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-65") + " Coins!");
+/*  88 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-65"));
+/*     */       }
+/*  90 */       if (kills == 50)
+/*     */       {
+/*  92 */         Bukkit.broadcastMessage(ChatColor.GREEN + killer.getName() + 
+/*  93 */           ChatColor.DARK_RED + " is on a 50 killstreak!");
+/*  94 */         killer.sendMessage(ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-50") + " Coins!");
+/*  95 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-50"));
+/*     */       }
+/*  97 */       if (kills == 30)
+/*     */       {
+/*  99 */         Bukkit.broadcastMessage(ChatColor.GREEN + killer.getName() + 
+/* 100 */           ChatColor.RED + " is on a 30 killstreak!");
+/* 101 */         killer.sendMessage(ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-30") + " Coins!");
+/* 102 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-30"));
+/*     */       }
+/* 104 */       if (kills == 20)
+/*     */       {
+/* 106 */         Bukkit.broadcastMessage(ChatColor.GREEN + killer.getName() + 
+/* 107 */           ChatColor.RED + " is on a 20 kill streak!");
+/* 108 */         killer.sendMessage(ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-20") + " Coins!");
+/* 109 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-20"));
+/*     */       }
+/* 111 */       if (kills == 10)
+/*     */       {
+/* 113 */         Bukkit.broadcastMessage(String.valueOf(ChatColor.YELLOW + killer.getName() + 
+/* 114 */           ChatColor.YELLOW + " is on a 10 kill streak"));
+/* 115 */         killer.sendMessage(ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-10") + " Coins!");
+/* 116 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-10"));
+/*     */       }
+/* 118 */       if (kills == 5)
+/*     */       {
+/* 120 */         Bukkit.broadcastMessage(String.valueOf(ChatColor.GREEN + killer.getName() + 
+/* 121 */           ChatColor.GREEN + " is on a 5 kill streak"));
+/* 122 */         killer.sendMessage(ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-5") + " Coins!");
+/* 123 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-5"));
+/*     */       }
+/*     */       
+/* 126 */       if ((kills == 1) && (Join.game.contains(killer)))
+/*     */       {
+/* 128 */         killstreak.put(name, Integer.valueOf(1));
+/* 129 */         killer.playSound(killer.getLocation(), Sound.valueOf(this.main.getConfig().getString("Sound.Streak")), 1.0F, 1.0F);
+/* 130 */         killer.sendMessage(String.valueOf(this.main.getConfig().getString("Prefix").replace("&", "ยง")) + " You're on a " + ChatColor.RED + "1" + ChatColor.WHITE + " Killstreak.");
+/*     */       }
+/*     */     }
+/*     */   }
+/*     */ }
 
 
-
-	import java.util.HashMap;
-	import java.util.Map;
-	import org.bukkit.Bukkit;
-	import org.bukkit.ChatColor;
-	import org.bukkit.EntityEffect;
-	import org.bukkit.Server;
-	import org.bukkit.Sound;
-	import org.bukkit.entity.Player;
-	import org.bukkit.event.EventHandler;
-	import org.bukkit.event.Listener;
-	import org.bukkit.event.entity.PlayerDeathEvent;
-	import org.bukkit.plugin.PluginManager;
-	import org.bukkit.plugin.java.JavaPlugin;
-	import org.bukkit.potion.PotionEffect;
-	import org.bukkit.potion.PotionEffectType;
-
-import me.RafaelAulerDeMeloAraujo.Coins.Coins;
-import me.RafaelAulerDeMeloAraujo.SpecialAbility.Join;
-import me.RafaelAulerDeMeloAraujo.main.Main;
-
-	public class Streak   implements Listener
-	 {
-	 private Main main;
-	 /*     */   static Main plugin;
-	 /*     */   
-	 /*     */   public Streak() {
-	 /*  27 */     this.main = main;
-	 /*  28 */     plugin = main;
-	 /*     */   }
-	
-	  public void onEnable()
-	  {
-	    
-	  }
-	  
-	  public static Map<String, Integer> killstreak = new HashMap();
-	  
-	  @EventHandler
-	  public void playerdeath(PlayerDeathEvent ev)
-	  {
-	    Player p = ev.getEntity();
-	    if ((p.getKiller() instanceof Player))
-	    {
-	    	 
-	      Player killer = p.getKiller();
-	      if ((Join.game.contains(killer)))
-	 	    
-	      p.sendMessage(ChatColor.RED + "Your killstreak has been destroyed by " + ChatColor.YELLOW + killer.getName());
-	      addtokillstreak(killer);
-	      this.killstreak.put(p.getName(), Integer.valueOf(0));
-	    }
-	  }
-	  
-	  
-	  public void addtokillstreak(Player killer)
-	  {
-	    String name = killer.getName();
-	    if (this.killstreak.containsKey(name))
-	    {
-	      int kills = ((Integer)this.killstreak.get(name)).intValue();
-	      kills++;
-	      this.killstreak.put(name, Integer.valueOf(kills));
-	      
-	      killer.sendMessage(ChatColor.GREEN + "You are on " + ChatColor.RED + Integer.toString(kills) + ChatColor.GREEN + " Killstreak.");
-	      if (kills == 100)
-	      {
-	        Bukkit.broadcastMessage(String.valueOf( ChatColor.GREEN+ killer.getName() + 
-	          ChatColor.DARK_GREEN + " is on a 100 killstreak!"));
-	        killer.sendMessage(ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-100") + " Coins!");
-	        Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-100"));
-	      }
-	      if (kills == 80)
-	      {
-	        Bukkit.broadcastMessage(ChatColor.GREEN+ killer.getName() + 
-	          ChatColor.DARK_GREEN+ " is on a 80 killstreak!");
-	        killer.sendMessage(ChatColor.GOLD+ "You win " + Main.customization.getDouble("KS-80") + " Coins!");
-	        Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-80"));
-	      }
-	      if (kills == 65)
-	      {
-	        Bukkit.broadcastMessage(ChatColor.GREEN+ killer.getName() + 
-	          ChatColor.DARK_GREEN + " is on a 65 killstreak!");
-	        killer.sendMessage(ChatColor.GOLD +  "You win " + Main.customization.getDouble("KS-65") + " Coins!");
-	        Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-65"));
-	      }
-	      if (kills == 50)
-	      {
-	        Bukkit.broadcastMessage(ChatColor.GREEN + killer.getName() + 
-	          ChatColor.DARK_RED + " is on a 50 killstreak!");
-	        killer.sendMessage(ChatColor.GOLD +  "You win " + Main.customization.getDouble("KS-50") + " Coins!");
-	        Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-50"));
-	      }
-	      if (kills == 30)
-	      {
-	        Bukkit.broadcastMessage( ChatColor.GREEN+ killer.getName() + 
-	          ChatColor.RED+  " is on a 30 killstreak!");
-	        killer.sendMessage(ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-30") + " Coins!");
-	        Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-30"));
-	      }
-	      if (kills == 20)
-	      {
-	        Bukkit.broadcastMessage(ChatColor.GREEN + killer.getName() + 
-	          ChatColor.RED +  " is on a 20 kill streak!");
-	        killer.sendMessage(ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-20") + " Coins!");
-	        Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-20"));
-	      }
-	      if (kills == 10)
-	      {
-	        Bukkit.broadcastMessage(String.valueOf( ChatColor.YELLOW + killer.getName() + 
-	          ChatColor.YELLOW +  " is on a 10 kill streak"));
-	        killer.sendMessage(ChatColor.GOLD+  "You win " + Main.customization.getDouble("KS-10") + " Coins!");
-	        Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-10"));
-	      }
-	      if (kills == 5)
-	      {
-	        Bukkit.broadcastMessage(String.valueOf( ChatColor.GREEN + killer.getName() + 
-	          ChatColor.GREEN +  " is on a 5 kill streak"));
-	        killer.sendMessage(ChatColor.GOLD+  "You win " + Main.customization.getDouble("KS-5") + " Coins!");
-	        Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-5"));
-	      }
-	    
-	    if (kills == 1 && Join.game.contains(killer))
-	    {
-	      this.killstreak.put(name, Integer.valueOf(1));
-	      killer.playSound(killer.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1.0F, 1.0F);
-	      killer.sendMessage(String.valueOf(this.main.getConfig().getString("Prefix").replace("&", "ง")) + " You're on a " + ChatColor.RED + "1" + ChatColor.WHITE + " Killstreak.");
-	    }
-	  }
-	}
-	 }
-
+/* Location:              D:\Desktop\video\Minhas Coisas do Desktop\KP-PVPvB12 (1).jar!\me\RafaelAulerDeMeloAraujo\ScoreboardManager\Streak.class
+ * Java compiler version: 8 (52.0)
+ * JD-Core Version:       0.7.1
+ */

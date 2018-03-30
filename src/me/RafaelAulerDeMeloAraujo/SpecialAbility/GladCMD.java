f542dc02fb9b9385da06e1d7e5dacdf888785976
RAFAELESPADA <34714331+RAFAELESPADA@users.noreply.github.com>
2018-03-30 15:24:14 -0300

    Update GladCMD.java

--- src/me/RafaelAulerDeMeloAraujo/SpecialAbility/GladCMD.java
+++ src/me/RafaelAulerDeMeloAraujo/SpecialAbility/GladCMD.java
@@ -1,100 +1,104 @@
-package me.RafaelAulerDeMeloAraujo.SpecialAbility;
+/*    */ package me.RafaelAulerDeMeloAraujo.SpecialAbility;
+/*    */ 
+/*    */ import me.RafaelAulerDeMeloAraujo.main.Main;
+/*    */ import org.bukkit.Material;
+/*    */ import org.bukkit.Sound;
+/*    */ import org.bukkit.command.Command;
+/*    */ import org.bukkit.configuration.file.FileConfiguration;
+/*    */ import org.bukkit.entity.Player;
+/*    */ import org.bukkit.inventory.ItemStack;
+/*    */ import org.bukkit.inventory.PlayerInventory;
+/*    */ import org.bukkit.inventory.meta.ItemMeta;
+/*    */ 
+/*    */ public class GladCMD implements org.bukkit.command.CommandExecutor
+/*    */ {
+/*    */   private Main main;
+/*    */   static Main plugin;
+/*    */   
+/*    */   public GladCMD(Main main)
+/*    */   {
+/* 20 */     this.main = main;
+/* 21 */     plugin = main;
+/*    */   }
+/*    */   
+/*    */ 
+/*    */   public boolean onCommand(org.bukkit.command.CommandSender sender, Command cmd, String commandLabel, String[] args)
+/*    */   {
+/* 27 */     Player p = (Player)sender;
+/*    */     
+/* 29 */     if (cmd.getName().equalsIgnoreCase("gladiator"))
+/*    */     {
+/* 31 */       if (!Join.game.contains(p.getName()))
+/*    */       {
+/* 33 */         p.sendMessage(String.valueOf(this.main.getConfig().getString("Prefix").replace("&", "Â§")) + " Â§eYou are not in kitpvp to do choose this kit!");
+/* 34 */         return true;
+/*    */       }
+/* 36 */       if (!p.hasPermission("kitpvp.kit.gladiator")) {
+/* 37 */         p.sendMessage(String.valueOf(this.main.getConfig().getString("Prefix").replace("&", "Â§")) + this.main.getConfig().getString("Permission").replace("&", "Â§").replaceAll("%permisson%", commandLabel));
+/* 38 */         p.playSound(p.getLocation(), Sound.valueOf(this.main.getConfig().getString("Sound.NoPermissionMessage")), 1.0F, 1.0F);
+/* 39 */         return true;
+/*    */       }
+/*    */       
+/*    */ 
+/*    */ 
+/* 44 */       if (Habilidade.ContainsAbility(p)) {
+/* 45 */         p.sendMessage(String.valueOf(this.main.getConfig().getString("Prefix").replace("&", "Â§")) + this.main.getConfig().getString("Message.KitUse").replace("&", "Â§"));
+/* 46 */         p.playSound(p.getLocation(), Sound.valueOf(this.main.getConfig().getString("Sound.KitUse")), 1.0F, 1.0F);
+/* 47 */         return true;
+/*    */       }
+/* 49 */       if (!Join.game.contains(p.getName()))
+/*    */       {
+/* 51 */         p.sendMessage(String.valueOf(this.main.getConfig().getString("Prefix").replace("&", "Â§")) + " Â§eYou are not in kitpvp to do choose this kit!");
+/* 52 */         return true;
+/*    */       }
+/* 54 */       p.getInventory().clear();
+/* 55 */       ItemStack dima = new ItemStack(Material.DIAMOND_SWORD);
+/* 56 */       ItemMeta souperaa = dima.getItemMeta();
+/* 57 */       souperaa.setDisplayName("Â§cSword");
+/* 58 */       dima.setItemMeta(souperaa);
+/* 59 */       ItemStack sopa = new ItemStack(Material.MUSHROOM_SOUP);
+/* 60 */       ItemMeta sopas = sopa.getItemMeta();
+/* 61 */       sopas.setDisplayName("Â§6Soup");
+/* 62 */       sopa.setItemMeta(sopas);
+/* 63 */       ItemStack especial = new ItemStack(Material.IRON_FENCE);
+/* 64 */       ItemMeta especial2 = especial.getItemMeta();
+/* 65 */       especial2.setDisplayName("Â§bGladiator Arena");
+/* 66 */       especial.setItemMeta(especial2);
+/*    */       
+/*    */ 
+/* 69 */       ItemStack capacete0 = new ItemStack(Material.IRON_HELMET);
+/*    */       
+/* 71 */       ItemStack peitoral0 = new ItemStack(Material.IRON_CHESTPLATE);
+/*    */       
+/* 73 */       ItemStack calca0 = new ItemStack(Material.IRON_LEGGINGS);
+/*    */       
+/* 75 */       ItemStack Bota0 = new ItemStack(Material.IRON_BOOTS);
+/*    */       
+/* 77 */       p.getInventory().setHelmet(capacete0);
+/* 78 */       p.getInventory().setChestplate(peitoral0);
+/* 79 */       p.getInventory().setLeggings(calca0);
+/* 80 */       p.getInventory().setBoots(Bota0);
+/*    */       
+/* 82 */       p.sendMessage(String.valueOf(this.main.getConfig().getString("Prefix").replace("&", "Â§")) + this.main.getConfig().getString("Message.Kit").replaceAll("%kit%", "Gladiator").replace("&", "Â§"));
+/*    */       
+/* 84 */       p.getInventory().addItem(new ItemStack[] { dima });
+/* 85 */       p.getInventory().addItem(new ItemStack[] { especial });
+/*    */       
+/*    */ 
+/* 88 */       Habilidade.setAbility(p, "Gladiator");
+/* 89 */       for (int i = 0; i <= 34; i++) {
+/* 90 */         p.getInventory().addItem(new ItemStack[] { sopa });
+/* 91 */         me.RafaelAulerDeMeloAraujo.TitleAPI.TitleAPI.sendTitle(p, Integer.valueOf(5), Integer.valueOf(20), Integer.valueOf(5), this.main.getConfig().getString("Title.KitTitle"), this.main.getConfig().getString("Title.KitSubTitle").replaceAll("%kit%", "Gladiator"));
+/* 92 */         p.playSound(p.getLocation(), Sound.valueOf(this.main.getConfig().getString("Sound.KitUse")), 1.0F, 1.0F);
+/*    */       }
+/*    */     }
+/*    */     
+/* 96 */     return false;
+/*    */   }
+/*    */ }
 
-import org.bukkit.Material;
-import org.bukkit.Sound;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
-import me.RafaelAulerDeMeloAraujo.main.Main;
-
-public class GladCMD implements CommandExecutor {
-	 private Main main;
-	 /*     */   static Main plugin;
-	 /*     */   
-	 /*     */   public GladCMD (Main main) {
-	 /*  27 */     this.main = main;
-	 /*  28 */     plugin = main;
-	 /*     */   }
-
-	 public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-	 /*     */   
-
- Player p = (Player)sender;
-
- if (cmd.getName().equalsIgnoreCase("gladiator"))
- {
-	  if (!Join.game.contains(p.getName()))
-      {
-          p.sendMessage(String.valueOf(this.main.getConfig().getString("Prefix").replace("&", "§")) + " §eYou are not in kitpvp to do choose this kit!");
-          return true;
-      }
- if (!p.hasPermission("kitpvp.kit.gladiator")) {
-	 p.sendMessage(String.valueOf(this.main.getConfig().getString("Prefix").replace("&", "§")) + this.main.getConfig().getString("Permission").replace("&", "§").replaceAll("%permisson%", commandLabel));
-   return true;
- }
-	  
- 
- 
-       if (Habilidade.ContainsAbility(p)) { 
-          p.sendMessage(String.valueOf(this.main.getConfig().getString("Prefix").replace("&", "§")) + this.main.getConfig().getString("Message.KitUse").replace("&", "§"));
-          p.playSound(p.getLocation(), Sound.valueOf(this.main.getConfig().getString("Sound.KitUse")), 1.0F, 1.0F);
-         return true;
-       }
-        if (!Join.game.contains(p.getName()))
-        {
-            p.sendMessage(String.valueOf(this.main.getConfig().getString("Prefix").replace("&", "§")) + " §eYou are not in kitpvp to do choose this kit!");
-            return true;
-        }
-        p.getInventory().clear();
-        ItemStack dima = new ItemStack(Material.DIAMOND_SWORD);
-        ItemMeta souperaa = dima.getItemMeta();
-        souperaa.setDisplayName("§cSword");
-        dima.setItemMeta(souperaa);
-        ItemStack sopa = new ItemStack(Material.MUSHROOM_SOUP);
-        ItemMeta sopas = sopa.getItemMeta();
-        sopas.setDisplayName("§6Soup");
-        sopa.setItemMeta(sopas);
-        ItemStack especial = new ItemStack(Material.IRON_FENCE);
-        ItemMeta especial2 = especial.getItemMeta();
-        especial2.setDisplayName("§bGladiator Arena");
-        especial.setItemMeta(especial2);
-
-        
-        ItemStack capacete0 = new ItemStack(Material.IRON_HELMET);
-        
-        ItemStack peitoral0 = new ItemStack(Material.IRON_CHESTPLATE);
-        
-        ItemStack calca0 = new ItemStack(Material.IRON_LEGGINGS);
-        
-        ItemStack Bota0 = new ItemStack(Material.IRON_BOOTS);
-        
-        p.getInventory().setHelmet(capacete0);
-        p.getInventory().setChestplate(peitoral0);
-        p.getInventory().setLeggings(calca0);
-        p.getInventory().setBoots(Bota0);
-   
-        p.sendMessage(String.valueOf(this.main.getConfig().getString("Prefix").replace("&", "§")) + this.main.getConfig().getString("Message.Kit").replaceAll("%kit%", "Gladiator").replace("&", "§"));
-  
-   p.getInventory().addItem(new ItemStack[] { dima });
-   p.getInventory().addItem(new ItemStack[] { especial });
-   
-   
-   Habilidade.setAbility(p, "Gladiator");
-   for (int i = 0; i <= 34; i++) {
-     p.getInventory().addItem(new ItemStack[] { sopa });
-     
-   }
-   
- }
- if (this.main.getConfig().getString("CustomKitTitleMessage").equalsIgnoreCase("true")) {
-	  /*  94 */        p.sendTitle(this.main.getConfig().getString("Title.KitTitle").replace("&", "§"), this.main.getConfig().getString("Title.KitSubTitle").replaceAll("%kit%", "Gladiator").replace("&", "§"));
-	  /*     */       }
-	  /*  96 */       return false; }{
- 
-}
-}
-
+/* Location:              D:\Desktop\video\Minhas Coisas do Desktop\KP-PVPvB12 (1).jar!\me\RafaelAulerDeMeloAraujo\SpecialAbility\GladCMD.class
+ * Java compiler version: 8 (52.0)
+ * JD-Core Version:       0.7.1
+ */

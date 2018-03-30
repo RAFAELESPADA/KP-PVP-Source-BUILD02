c3799fca8fc1c6d7b809e27ff45f6fb8f97e4e87
RAFAELESPADA <34714331+RAFAELESPADA@users.noreply.github.com>
2018-03-30 15:22:26 -0300

    Update Critical.java

--- src/me/RafaelAulerDeMeloAraujo/SpecialAbility/Critical.java
+++ src/me/RafaelAulerDeMeloAraujo/SpecialAbility/Critical.java
@@ -1,130 +1,132 @@
-package me.RafaelAulerDeMeloAraujo.SpecialAbility;
+/*     */ package me.RafaelAulerDeMeloAraujo.SpecialAbility;
+/*     */ 
+/*     */ import java.util.ArrayList;
+/*     */ import java.util.Random;
+/*     */ import me.RafaelAulerDeMeloAraujo.TitleAPI.TitleAPI;
+/*     */ import me.RafaelAulerDeMeloAraujo.main.Main;
+/*     */ import org.bukkit.ChatColor;
+/*     */ import org.bukkit.Effect;
+/*     */ import org.bukkit.Material;
+/*     */ import org.bukkit.Sound;
+/*     */ import org.bukkit.World;
+/*     */ import org.bukkit.command.Command;
+/*     */ import org.bukkit.command.CommandExecutor;
+/*     */ import org.bukkit.command.CommandSender;
+/*     */ import org.bukkit.configuration.file.FileConfiguration;
+/*     */ import org.bukkit.entity.Player;
+/*     */ import org.bukkit.event.EventHandler;
+/*     */ import org.bukkit.event.Listener;
+/*     */ import org.bukkit.event.entity.EntityDamageByEntityEvent;
+/*     */ import org.bukkit.inventory.ItemStack;
+/*     */ import org.bukkit.inventory.PlayerInventory;
+/*     */ import org.bukkit.inventory.meta.ItemMeta;
+/*     */ 
+/*     */ 
+/*     */ public class Critical
+/*     */   implements Listener, CommandExecutor
+/*     */ {
+/*     */   private Main main;
+/*     */   static Main plugin;
+/*     */   
+/*     */   public Critical(Main main)
+/*     */   {
+/*  33 */     this.main = main;
+/*  34 */     plugin = main;
+/*     */   }
+/*     */   
+/*     */   @EventHandler
+/*     */   public void dano(EntityDamageByEntityEvent e)
+/*     */   {
+/*  40 */     if (((e.getEntity() instanceof Player)) && ((e.getDamager() instanceof Player)))
+/*     */     {
+/*  42 */       Player p = (Player)e.getEntity();
+/*  43 */       Player d = (Player)e.getDamager();
+/*  44 */       if (Habilidade.getAbility(d) == "Critical")
+/*     */       {
+/*  46 */         Random r = new Random();
+/*  47 */         int c = r.nextInt(100);
+/*  48 */         if (c <= 10)
+/*     */         {
+/*  50 */           e.setDamage(e.getDamage() + 4.0D);
+/*  51 */           p.getWorld().playEffect(p.getLocation(), Effect.STEP_SOUND, Material.REDSTONE_BLOCK, 10);
+/*  52 */           d.sendMessage(ChatColor.RED + "You give a critical hit on player " + ChatColor.DARK_RED + p.getName());
+/*  53 */           p.sendMessage(ChatColor.RED + "You receive a critical hit from " + ChatColor.DARK_RED + d.getName());
+/*     */         }
+/*     */       }
+/*     */     }
+/*     */   }
+/*     */   
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */   public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
+/*     */   {
+/*  65 */     Player p = (Player)sender;
+/*  66 */     if (cmd.getName().equalsIgnoreCase("critical"))
+/*     */     {
+/*     */ 
+/*  69 */       if (!p.hasPermission("kitpvp.kit.critical"))
+/*     */       {
+/*  71 */         p.sendMessage(String.valueOf(this.main.getConfig().getString("Prefix").replace("&", "Â§")) + this.main.getConfig().getString("Permission").replace("&", "Â§").replaceAll("%permisson%", commandLabel));
+/*  72 */         p.playSound(p.getLocation(), Sound.valueOf(this.main.getConfig().getString("Sound.NoPermissionMessage")), 1.0F, 1.0F);
+/*  73 */         return true;
+/*     */       }
+/*  75 */       if (!Join.game.contains(p.getName()))
+/*     */       {
+/*  77 */         p.sendMessage(String.valueOf(this.main.getConfig().getString("Prefix").replace("&", "Â§")) + " Â§eYou are not in kitpvp to choose this kit!");
+/*  78 */         return true;
+/*     */       }
+/*     */       
+/*  81 */       if (Habilidade.ContainsAbility(p)) {
+/*  82 */         p.sendMessage(String.valueOf(this.main.getConfig().getString("Prefix").replace("&", "Â§")) + this.main.getConfig().getString("Message.KitUse").replace("&", "Â§"));
+/*  83 */         p.playSound(p.getLocation(), Sound.valueOf(this.main.getConfig().getString("Sound.KitUse")), 1.0F, 1.0F);
+/*  84 */         return true;
+/*     */       }
+/*  86 */       p.getInventory().clear();
+/*  87 */       ItemStack dima = new ItemStack(Material.DIAMOND_SWORD);
+/*  88 */       ItemMeta souperaa = dima.getItemMeta();
+/*  89 */       souperaa.setDisplayName("Â§cSword");
+/*  90 */       dima.setItemMeta(souperaa);
+/*  91 */       ItemStack sopa = new ItemStack(Material.MUSHROOM_SOUP);
+/*  92 */       ItemMeta sopas = sopa.getItemMeta();
+/*  93 */       sopas.setDisplayName("Â§6Soup");
+/*  94 */       sopa.setItemMeta(sopas);
+/*     */       
+/*     */ 
+/*  97 */       ItemStack capacete0 = new ItemStack(Material.IRON_HELMET);
+/*     */       
+/*  99 */       ItemStack peitoral0 = new ItemStack(Material.IRON_CHESTPLATE);
+/*     */       
+/* 101 */       ItemStack calca0 = new ItemStack(Material.IRON_LEGGINGS);
+/*     */       
+/* 103 */       ItemStack Bota0 = new ItemStack(Material.IRON_BOOTS);
+/*     */       
+/* 105 */       p.getInventory().setHelmet(capacete0);
+/* 106 */       p.getInventory().setChestplate(peitoral0);
+/* 107 */       p.getInventory().setLeggings(calca0);
+/* 108 */       p.getInventory().setBoots(Bota0);
+/*     */       
+/* 110 */       p.sendMessage(String.valueOf(this.main.getConfig().getString("Prefix").replace("&", "Â§")) + this.main.getConfig().getString("Message.Kit").replaceAll("%kit%", "Critical").replace("&", "Â§"));
+/* 111 */       Habilidade.setAbility(p, "Critical");
+/* 112 */       p.getInventory().addItem(new ItemStack[] { dima });
+/*     */       
+/*     */ 
+/*     */ 
+/*     */ 
+/* 117 */       for (int i = 0; i <= 34; i++) {
+/* 118 */         p.getInventory().addItem(new ItemStack[] { sopa });
+/* 119 */         p.playSound(p.getLocation(), Sound.valueOf(this.main.getConfig().getString("Sound.KitUse")), 1.0F, 1.0F);
+/* 120 */         TitleAPI.sendTitle(p, Integer.valueOf(5), Integer.valueOf(20), Integer.valueOf(5), this.main.getConfig().getString("Title.KitTitle"), this.main.getConfig().getString("Title.KitSubTitle").replaceAll("%kit%", "Critical"));
+/*     */       }
+/*     */     }
+/*     */     
+/* 124 */     return false;
+/*     */   }
+/*     */ }
 
 
-import java.util.Random;
-
-import org.bukkit.ChatColor;
-import org.bukkit.Effect;
-import org.bukkit.GameMode;
-import org.bukkit.Material;
-import org.bukkit.Sound;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-
-import me.RafaelAulerDeMeloAraujo.main.Main;
-
-
-
-public class Critical 
-implements Listener, CommandExecutor
-{
-	 private Main main;
-	 /*     */   static Main plugin;
-	 /*     */   
-	 /*     */   public Critical(Main main) {
-	 /*  27 */     this.main = main;
-	 /*  28 */     plugin = main;
-	 /*     */   }
-
-	@EventHandler
-	  public void dano(EntityDamageByEntityEvent e)
-	  {
-	    if (((e.getEntity() instanceof Player)) && ((e.getDamager() instanceof Player)))
-	    {
-	      Player p = (Player)e.getEntity();
-	      Player d = (Player)e.getDamager();
-	      if (Habilidade.getAbility(d) == "Critical")
-	      {
-	        Random r = new Random();
-	        int c = r.nextInt(100);
-	        if (c <= 10)
-	        {
-	          e.setDamage(e.getDamage() + 4.0D);
-	          p.getWorld().playEffect(p.getLocation(), Effect.STEP_SOUND, Material.REDSTONE_BLOCK, 10);
-	          d.sendMessage(ChatColor.RED + "You give a critical hit on player " + ChatColor.DARK_RED + p.getName());
-	          p.sendMessage(ChatColor.RED + "You receive a critical hit from " + ChatColor.DARK_RED + d.getName());
-	        }
-	      }
-	    }
-	  }
-	    
-	   {
-		   
-	   }
-		   public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-				 /*     */   
-
-			  Player p = (Player)sender;
-	  if (cmd.getName().equalsIgnoreCase("critical"))
-	  {
-		 
-	    if (!p.hasPermission("kitpvp.kit.critical"))
-	    {
-	    	p.sendMessage(String.valueOf(this.main.getConfig().getString("Prefix").replace("&", "§")) + this.main.getConfig().getString("Permission").replace("&", "§").replaceAll("%permisson%", commandLabel));
-	      return true;
-	    }
-	    if (!Join.game.contains(p.getName()))
-        {
-            p.sendMessage(String.valueOf(this.main.getConfig().getString("Prefix").replace("&", "§")) + " §eYou are not in kitpvp to choose this kit!");
-            return true;
-        }
-	  
-	    if (Habilidade.ContainsAbility(p)) { 
-	          p.sendMessage(String.valueOf(this.main.getConfig().getString("Prefix").replace("&", "§")) + this.main.getConfig().getString("Message.KitUse").replace("&", "§"));
-	          p.playSound(p.getLocation(), Sound.valueOf(this.main.getConfig().getString("Sound.KitUse")), 1.0F, 1.0F);
-	         return true; 
-	    }
-	         p.getInventory().clear();
-	         ItemStack dima = new ItemStack(Material.DIAMOND_SWORD);
-	         ItemMeta souperaa = dima.getItemMeta();
-	         souperaa.setDisplayName("§cSword");
-	         dima.setItemMeta(souperaa);
-	         ItemStack sopa = new ItemStack(Material.MUSHROOM_SOUP);
-	         ItemMeta sopas = sopa.getItemMeta();
-	         sopas.setDisplayName("§6Soup");
-	         sopa.setItemMeta(sopas);
-	         
-	         
-	         ItemStack capacete0 = new ItemStack(Material.IRON_HELMET);
-	         
-	         ItemStack peitoral0 = new ItemStack(Material.IRON_CHESTPLATE);
-	         
-	         ItemStack calca0 = new ItemStack(Material.IRON_LEGGINGS);
-	         
-	         ItemStack Bota0 = new ItemStack(Material.IRON_BOOTS);
-	         
-	         p.getInventory().setHelmet(capacete0);
-	         p.getInventory().setChestplate(peitoral0);
-	         p.getInventory().setLeggings(calca0);
-	         p.getInventory().setBoots(Bota0);
-	    
-	    p.sendMessage(String.valueOf(this.main.getConfig().getString("Prefix").replace("&", "§")) + this.main.getConfig().getString("Message.Kit").replaceAll("%kit%", "Critical").replace("&", "§"));
-	    Habilidade.setAbility(p, "Critical");
-	    p.getInventory().addItem(new ItemStack[] { dima });
-	    
-	    
-	    
-	    
-	    for (int i = 0; i <= 34; i++) {
-	      p.getInventory().addItem(new ItemStack[] { sopa });
-	      
-	    }
-	    
-	  }
-	  if (this.main.getConfig().getString("CustomKitTitleMessage").equalsIgnoreCase("true")) {
-		  /*  94 */         p.sendTitle(this.main.getConfig().getString("Title.KitTitle").replace("&", "§"), this.main.getConfig().getString("Title.KitSubTitle").replaceAll("%kit%", "Critical").replace("&", "§"));
-		  /*     */       }
-		  /*  96 */       return false; }{
-	  
-	}
-	
-{
-}
-}
+/* Location:              D:\Desktop\video\Minhas Coisas do Desktop\KP-PVPvB12 (1).jar!\me\RafaelAulerDeMeloAraujo\SpecialAbility\Critical.class
+ * Java compiler version: 8 (52.0)
+ * JD-Core Version:       0.7.1
+ */

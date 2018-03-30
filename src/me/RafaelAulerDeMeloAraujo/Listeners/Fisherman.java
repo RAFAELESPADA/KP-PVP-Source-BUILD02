c0075e4467e152684740e728460dfc854861b2f6
RAFAELESPADA <34714331+RAFAELESPADA@users.noreply.github.com>
2018-03-30 15:18:41 -0300

    Update Fisherman.java

--- src/me/RafaelAulerDeMeloAraujo/Listeners/Fisherman.java
+++ src/me/RafaelAulerDeMeloAraujo/Listeners/Fisherman.java
@@ -1,44 +1,45 @@
 /*    */ package me.RafaelAulerDeMeloAraujo.Listeners;
-import me.RafaelAulerDeMeloAraujo.SpecialAbility.Habilidade;
 /*    */ 
+/*    */ import me.RafaelAulerDeMeloAraujo.SpecialAbility.Habilidade;
 /*    */ import me.RafaelAulerDeMeloAraujo.main.Main;
-
-import org.bukkit.Effect;
 /*    */ import org.bukkit.Material;
 /*    */ import org.bukkit.Sound;
-/*    */ 
+/*    */ import org.bukkit.configuration.file.FileConfiguration;
 /*    */ import org.bukkit.entity.Player;
 /*    */ import org.bukkit.event.EventHandler;
+/*    */ import org.bukkit.event.Listener;
 /*    */ import org.bukkit.event.player.PlayerFishEvent;
+/*    */ import org.bukkit.inventory.ItemStack;
 /*    */ 
-/*    */ public class Fisherman implements org.bukkit.event.Listener
+/*    */ public class Fisherman implements Listener
 /*    */ {
 /*    */   private Main main;
 /*    */   
 /*    */   public Fisherman(Main main)
 /*    */   {
-/* 17 */     this.main = main;
+/* 20 */     this.main = main;
 /*    */   }
 /*    */   
+/*    */ 
+/*    */ 
 /*    */   @EventHandler
 /*    */   public void onPlayerHitFishingrodscorpion(PlayerFishEvent event)
 /*    */   {
-
-
-/* 23 */     Player player = event.getPlayer();
-/* 24 */     if ((event.getCaught() instanceof Player)) {
-/* 25 */       Player caught = (Player)event.getCaught();
-/* 26 */       if (player.getItemInHand().getType() == Material.FISHING_ROD &&  ((Habilidade.getAbility(player) == "Fisherman"))) {
-/* 27 */         player.getWorld().playEffect(player.getLocation().add(0.0D, 1.5D, 0.0D), Effect.MOBSPAWNER_FLAMES, 1);
-/* 28 */         caught.teleport(player.getLocation());
-/* 29 */         caught.playSound(caught.getLocation(), Sound.valueOf(this.main.getConfig().getString("Sound.Fisherman")), 1.0F, 1.0F);
-/* 30 */         player.playSound(player.getLocation(), Sound.valueOf(this.main.getConfig().getString("Sound.Fisherman")), 1.0F, 1.0F);
+/* 28 */     Player player = event.getPlayer();
+/* 29 */     if ((event.getCaught() instanceof Player)) {
+/* 30 */       Player caught = (Player)event.getCaught();
+/* 31 */       if ((player.getItemInHand().getType() == Material.FISHING_ROD) && (Habilidade.getAbility(player) == "Fisherman")) {
+/* 32 */         player.getWorld().playEffect(player.getLocation().add(0.0D, 1.5D, 0.0D), org.bukkit.Effect.MOBSPAWNER_FLAMES, 1);
+/* 33 */         caught.teleport(player.getLocation());
+/* 34 */         caught.playSound(caught.getLocation(), Sound.valueOf(this.main.getConfig().getString("Sound.Fisherman")), 1.0F, 1.0F);
+/* 35 */         player.playSound(player.getLocation(), Sound.valueOf(this.main.getConfig().getString("Sound.Fisherman")), 1.0F, 1.0F);
 /*    */       }
 /*    */     }
- 
+/*    */   }
+/*    */ }
 
 
-}
-}
-
-
+/* Location:              D:\Desktop\video\Minhas Coisas do Desktop\KP-PVPvB12 (1).jar!\me\RafaelAulerDeMeloAraujo\Listeners\Fisherman.class
+ * Java compiler version: 8 (52.0)
+ * JD-Core Version:       0.7.1
+ */

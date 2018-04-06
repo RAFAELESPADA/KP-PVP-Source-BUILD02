71ee199d0a2fca2c82ad425fb37d85c6160c0828
RAFAELESPADA <34714331+RAFAELESPADA@users.noreply.github.com>
2018-04-06 20:34:09 -0300

    Update NoDrops.java

--- src/me/RafaelAulerDeMeloAraujo/Listeners/NoDrops.java
+++ src/me/RafaelAulerDeMeloAraujo/Listeners/NoDrops.java
@@ -1,57 +1,65 @@
 /*    */ package me.RafaelAulerDeMeloAraujo.Listeners;
-import me.RafaelAulerDeMeloAraujo.SpecialAbility.Join;
 /*    */ 
+/*    */ import java.util.ArrayList;
+/*    */ import me.RafaelAulerDeMeloAraujo.SpecialAbility.Join;
 /*    */ import me.RafaelAulerDeMeloAraujo.main.Main;
 /*    */ import org.bukkit.Material;
-import org.bukkit.Sound;
-/*    */ import org.bukkit.entity.Item;
-import org.bukkit.event.EventHandler;
+/*    */ import org.bukkit.entity.Player;
 /*    */ import org.bukkit.event.player.PlayerDropItemEvent;
-import org.bukkit.event.player.PlayerPickupItemEvent;
-/*    */ import org.bukkit.inventory.ItemStack;
+/*    */ import org.bukkit.event.player.PlayerPickupItemEvent;
 /*    */ 
 /*    */ public class NoDrops implements org.bukkit.event.Listener
 /*    */ {
-	private Main main;
-	
-		/*     */   
-/*    */   public NoDrops(Main main) {}
+/*    */   private Main main;
 /*    */   
-/*    */   
-@EventHandler
-public void onPlayerDropItem(PlayerDropItemEvent paramPlayerPickupItemEvent)
-{
-	if (!Join.game.contains(paramPlayerPickupItemEvent.getPlayer().getName()))
-		return;
-	{
-	if (Join.game.contains(paramPlayerPickupItemEvent.getPlayer().getName()))
-	{
-	if (paramPlayerPickupItemEvent.getItemDrop().getItemStack().getType() == Material.BOWL)
-	{
-    paramPlayerPickupItemEvent.setCancelled(false);
-    paramPlayerPickupItemEvent.getPlayer().playSound(paramPlayerPickupItemEvent.getPlayer().getLocation(),Sound.ENTITY_ITEM_PICKUP, 10.0F, 30.0F);}
-    else 
-  	  paramPlayerPickupItemEvent.setCancelled(true);  
-  }
-	}
-}
+/*    */   public NoDrops(Main main)
+/*    */   {
+/* 17 */     this.main = main;
+/*    */   }
 /*    */   
 /*    */ 
-@EventHandler
-public void onPlayerPickupItem(PlayerPickupItemEvent paramPlayerPickupItemEvent)
-{
-	if (!Join.game.contains(paramPlayerPickupItemEvent.getPlayer().getName()))
-		return;
-	{
-	if (Join.game.contains(paramPlayerPickupItemEvent.getPlayer().getName()))
-	{
-	if (paramPlayerPickupItemEvent.getItem().getItemStack().getType() == Material.BOWL || paramPlayerPickupItemEvent.getItem().getItemStack().getType() == Material.MUSHROOM_SOUP)
-	{
-    paramPlayerPickupItemEvent.setCancelled(false); }
-    else 
-  	  paramPlayerPickupItemEvent.setCancelled(true);  
-  }
-	}
-}
-}
+/*    */ 
+/*    */ 
+/*    */   @org.bukkit.event.EventHandler
+/*    */   public void onPlayerDropItem(PlayerDropItemEvent paramPlayerPickupItemEvent)
+/*    */   {
+/* 26 */     if (!Join.game.contains(paramPlayerPickupItemEvent.getPlayer().getName())) {
+/* 27 */       return;
+/*    */     }
+/* 29 */     if (Join.game.contains(paramPlayerPickupItemEvent.getPlayer().getName()))
+/*    */     {
+/* 31 */       if (paramPlayerPickupItemEvent.getItemDrop().getItemStack().getType() == Material.BOWL)
+/*    */       {
+/* 33 */         paramPlayerPickupItemEvent.setCancelled(false);
+/* 34 */         paramPlayerPickupItemEvent.getPlayer().playSound(paramPlayerPickupItemEvent.getPlayer().getLocation(), org.bukkit.Sound.valueOf(this.main.getConfig().getString("Sound.BowlDrop")), 1.0F, 1.0F);
+/*    */       }
+/*    */       else {
+/* 37 */         paramPlayerPickupItemEvent.setCancelled(true);
+/*    */       }
+/*    */     }
+/*    */   }
+/*    */   
+/*    */ 
+/*    */   @org.bukkit.event.EventHandler
+/*    */   public void onPlayerPickupItem(PlayerPickupItemEvent paramPlayerPickupItemEvent)
+/*    */   {
+/* 46 */     if (!Join.game.contains(paramPlayerPickupItemEvent.getPlayer().getName())) {
+/* 47 */       return;
+/*    */     }
+/* 49 */     if (Join.game.contains(paramPlayerPickupItemEvent.getPlayer().getName()))
+/*    */     {
+/* 51 */       if ((paramPlayerPickupItemEvent.getItem().getItemStack().getType() == Material.BOWL) || (paramPlayerPickupItemEvent.getItem().getItemStack().getType() == Material.MUSHROOM_SOUP))
+/*    */       {
+/* 53 */         paramPlayerPickupItemEvent.setCancelled(false);
+/*    */       } else {
+/* 55 */         paramPlayerPickupItemEvent.setCancelled(true);
+/*    */       }
+/*    */     }
+/*    */   }
+/*    */ }
 
+
+/* Location:              D:\Desktop\video\Minhas Coisas do Desktop\KP-PVPvB12 (1).jar!\me\RafaelAulerDeMeloAraujo\Listeners\NoDrops.class
+ * Java compiler version: 8 (52.0)
+ * JD-Core Version:       0.7.1
+ */

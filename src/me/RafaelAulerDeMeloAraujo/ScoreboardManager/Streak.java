4ea8d7f669527a8b30dbc0b6592e3abd80ea83c2
RAFAELESPADA <34714331+RAFAELESPADA@users.noreply.github.com>
2019-01-08 23:58:05 -0200

    Update Streak.java

--- src/me/RafaelAulerDeMeloAraujo/ScoreboardManager/Streak.java
+++ src/me/RafaelAulerDeMeloAraujo/ScoreboardManager/Streak.java
@@ -7,9 +7,11 @@
 import me.RafaelAulerDeMeloAraujo.SpecialAbility.API;
 /*     */ import me.RafaelAulerDeMeloAraujo.SpecialAbility.Join;
 /*     */ import me.RafaelAulerDeMeloAraujo.main.Main;
+import net.md_5.bungee.api.chat.BaseComponent;
 /*     */ import org.bukkit.Bukkit;
 /*     */ import org.bukkit.ChatColor;
 /*     */ import org.bukkit.Sound;
+import org.bukkit.World;
 /*     */ import org.bukkit.configuration.file.FileConfiguration;
 /*     */ import org.bukkit.entity.Player;
 /*     */ import org.bukkit.event.EventHandler;
@@ -55,7 +57,9 @@ import me.RafaelAulerDeMeloAraujo.SpecialAbility.API;
 /*  54 */       killstreak.put(p.getName(), Integer.valueOf(0));
 /*     */     }
 /*     */   }
-/*     */   
+/*     
+*/  
+
 /*     */ 
 /*     */   public void addtokillstreak(Player killer)
 /*     */   {
@@ -69,72 +73,127 @@ import me.RafaelAulerDeMeloAraujo.SpecialAbility.API;
 /*  68 */       killer.sendMessage(API.NomeServer + "" + ChatColor.GREEN + "You are on " + ChatColor.RED + Integer.toString(kills) + ChatColor.GREEN + " Killstreak.");
 /*  69 */       if (kills == 100)
 /*     */       {
-/*  71 */         Bukkit.broadcastMessage(String.valueOf(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
-/*  72 */           ChatColor.DARK_GREEN + " is on a 100 killstreak!"));
+	/*  99 */         broadcast(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
+			/*  93 */           ChatColor.DARK_RED + " is on a 100 killstreak!", killer.getWorld());
 /*  73 */         killer.sendMessage(API.NomeServer + "" + ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-100") + " Coins!");
 /*  74 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-100"));
 /*     */       }
+/*  76 */       if (kills == 90)
+/*     */       {
+	/*  99 */         broadcast(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
+			/*  93 */           ChatColor.DARK_RED + " is on a 90 killstreak!", killer.getWorld());
+/*  80 */         killer.sendMessage(API.NomeServer + "" + ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-90") + " Coins!");
+/*  81 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-90"));
+/*     */       }
 /*  76 */       if (kills == 80)
 /*     */       {
-/*  78 */         Bukkit.broadcastMessage(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
-/*  79 */           ChatColor.DARK_GREEN + " is on a 80 killstreak!");
+	/*  99 */         broadcast(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
+			/*  93 */           ChatColor.DARK_RED + " is on a 80 killstreak!", killer.getWorld());
 /*  80 */         killer.sendMessage(API.NomeServer + "" + ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-80") + " Coins!");
 /*  81 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-80"));
 /*     */       }
+if (kills == 70)
+/*     */       {
+	/*  99 */         broadcast(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
+			/*  93 */           ChatColor.DARK_RED + " is on a 70 killstreak!", killer.getWorld());
+/*  80 */         killer.sendMessage(API.NomeServer + "" + ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-70") + " Coins!");
+/*  81 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-70"));
+/*     */       }
 /*  83 */       if (kills == 65)
 /*     */       {
-/*  85 */         Bukkit.broadcastMessage(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
-/*  86 */           ChatColor.DARK_GREEN + " is on a 65 killstreak!");
+	/*  99 */         broadcast(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
+			/*  93 */           ChatColor.DARK_RED + " is on a 65 killstreak!", killer.getWorld());
 /*  87 */         killer.sendMessage(API.NomeServer + "" + ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-65") + " Coins!");
 /*  88 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-65"));
 /*     */       }
+/*  83 */       if (kills == 60)
+/*     */       {
+	/*  99 */         broadcast(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
+			/*  93 */           ChatColor.DARK_RED + " is on a 60 killstreak!", killer.getWorld());
+/*  87 */         killer.sendMessage(API.NomeServer + "" + ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-60") + " Coins!");
+/*  88 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-60"));
+/*     */       }
 /*  90 */       if (kills == 50)
 /*     */       {
-/*  92 */         Bukkit.broadcastMessage(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
-/*  93 */           ChatColor.DARK_RED + " is on a 50 killstreak!");
+	/*  99 */         broadcast(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
+			/*  93 */           ChatColor.DARK_RED + " is on a 50 killstreak!", killer.getWorld());
 /*  94 */         killer.sendMessage(API.NomeServer + "" + ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-50") + " Coins!");
 /*  95 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-50"));
 /*     */       }
+if (kills == 40)
+/*     */       {
+/*  99 */         broadcast(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
+/*  93 */           ChatColor.DARK_RED + " is on a 40 killstreak!", killer.getWorld());
+/* 101 */         killer.sendMessage(API.NomeServer + "" + ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-30") + " Coins!");
+/* 102 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-40"));
+/*     */       }
 /*  97 */       if (kills == 30)
 /*     */       {
-/*  99 */         Bukkit.broadcastMessage(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
-/* 100 */           ChatColor.RED + " is on a 30 killstreak!");
+	/*  99 */         broadcast(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
+			/*  93 */           ChatColor.DARK_RED + " is on a 30 killstreak!", killer.getWorld());
 /* 101 */         killer.sendMessage(API.NomeServer + "" + ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-30") + " Coins!");
 /* 102 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-30"));
 /*     */       }
+/*  97 */       if (kills == 25)
+/*     */       {
+	
+	/*  99 */         broadcast(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
+			/*  93 */           ChatColor.DARK_RED + " is on a 25 killstreak!", killer.getWorld());
+/* 101 */         killer.sendMessage(API.NomeServer + "" + ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-25") + " Coins!");
+/* 102 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-25"));
+/*     */       }
 /* 104 */       if (kills == 20)
 /*     */       {
-/* 106 */         Bukkit.broadcastMessage(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
-/* 107 */           ChatColor.RED + " is on a 20 kill streak!");
+	/*  99 */         broadcast(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
+			/*  93 */           ChatColor.DARK_RED + " is on a 20 killstreak!", killer.getWorld());
 /* 108 */         killer.sendMessage(API.NomeServer + "" + ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-20") + " Coins!");
 /* 109 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-20"));
 /*     */       }
+/* 104 */       if (kills == 15)
+/*     */       {
+	/*  99 */         broadcast(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
+			/*  93 */           ChatColor.DARK_RED + " is on a 15 killstreak!", killer.getWorld());
+/* 108 */         killer.sendMessage(API.NomeServer + "" + ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-15") + " Coins!");
+/* 109 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-15"));
+/*     */       }
 /* 111 */       if (kills == 10)
 /*     */       {
-/* 113 */         Bukkit.broadcastMessage(String.valueOf(API.NomeServer + "" + ChatColor.YELLOW + killer.getName() + 
-/* 114 */           ChatColor.YELLOW + " is on a 10 kill streak"));
+	/*  99 */         broadcast(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
+			/*  93 */           ChatColor.DARK_RED + " is on a 10 killstreak!", killer.getWorld());
 /* 115 */         killer.sendMessage(API.NomeServer + "" + ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-10") + " Coins!");
 /* 116 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-10"));
 /*     */       }
 /* 118 */       if (kills == 5)
 /*     */       {
-/* 120 */         Bukkit.broadcastMessage(String.valueOf(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
-/* 121 */           ChatColor.GREEN + " is on a 5 kill streak"));
+	/*  99 */         broadcast(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
+			/*  93 */           ChatColor.DARK_RED + " is on a 5 killstreak!", killer.getWorld());
 /* 122 */         killer.sendMessage(API.NomeServer + "" + ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-5") + " Coins!");
 /* 123 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-5"));
+/*     */       }
+/* 118 */       if (kills == 3)
+/*     */       {
+	/*  99 */         broadcast(API.NomeServer + "" + ChatColor.GREEN + killer.getName() + 
+			/*  93 */           ChatColor.DARK_RED + " is on a 3 killstreak!" , killer.getWorld());
+/* 122 */         killer.sendMessage(API.NomeServer + "" + ChatColor.GOLD + "You win " + Main.customization.getDouble("KS-3") + " Coins!");
+/* 123 */         Coins.addCoins(killer.getName(), Main.customization.getDouble("KS-3"));
 /*     */       }
 /*     */       
 /* 126 */       if ((kills == 1) && (Join.game.contains(killer)))
 /*     */       {
 /* 128 */         killstreak.put(name, Integer.valueOf(1));
-/* 129 */         killer.playSound(killer.getLocation(), Sound.valueOf(this.main.getConfig().getString("Sound.Streak")), 1.0F, 1.0F);
+/* 129 */         killer.playSound(killer.getLocation(), Sound.valueOf(this.main.getConfig().getString("Sound.Streak")), 3.0F, 1.0F);
 /* 130 */         killer.sendMessage(String.valueOf(this.main.getConfig().getString("Prefix").replace("&", "§")) + " You're on a " + ChatColor.RED + "1" + ChatColor.WHITE + " Killstreak.");
 /*     */       }
 /*     */     }
 /*     */   }
-/*     */ }
+/*     */ 
 
-
+  public void broadcast(String text, World w){
+    for(Player p: w.getPlayers()){
+        p.sendMessage(text);
+        p.playSound(p.getLocation(), Sound.valueOf(this.main.getConfig().getString("Sound.Streak")), 3.0F, 1.0F);
+    }
+  }}
 /* Location:              D:\Desktop\video\Minhas Coisas do Desktop\KP-PVPvB12 (1).jar!\me\RafaelAulerDeMeloAraujo\ScoreboardManager\Streak.class
  * Java compiler version: 8 (52.0)
  * JD-Core Version:       0.7.1
